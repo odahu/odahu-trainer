149952da353c4a82703347b5e875d68db562871f
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2019-11-08 12:36:36 +0300

    [#951] Publish all release NPM and Python packages to NPM and PyPi (#6)

--- setup.py
+++ setup.py
@@ -14,11 +14,33 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+import os
+import re
 
 from setuptools import setup, find_packages
 
+PACKAGE_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+VERSION_FILE = os.path.join(PACKAGE_ROOT_PATH, 'mlflowrunner', '__version__.py')
+
+
+def extract_version() -> str:
+    """
+    Extract version from .py file using regex
+
+    :return: legion version
+    """
+    with open(VERSION_FILE, 'rt') as version_file:
+        file_content = version_file.read()
+        VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+        mo = re.search(VSRE, file_content, re.M)
+        if mo:
+            return mo.group(1)
+        else:
+            raise RuntimeError("Unable to find version string in %s." % (file_content,))
+
+
 setup(
-    name='legion_mlflow_runner',
+    name='legion-mlflow-runner',
     author='Legion Platform Team',
     license='Apache v2',
     classifiers=[
@@ -34,11 +56,10 @@ setup(
         'console_scripts': ['legion-mlflow-runner=mlflowrunner.runner:main'],
     },
     install_requires=[
-        # TODO: fix dependencies when we will publish the legion packages to pypi repo
-        # TODO: consider Pipenv usage
+        # TODO: change to PyPi when we publish release
         'legion-sdk @ git+https://github.com/legion-platform/legion.git@1.0.0-rc18#egg=legion-sdk&subdirectory=legion/sdk',
         'mlflow<=1.3.0,>=1.0.0',
         'PyYAML>=3.1.2'
     ],
-    version='1.0.0'
+    version=extract_version()
 )
