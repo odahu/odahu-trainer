8f9edbd99d1cf353e20ed72df072ed8ce59fe075
Innokentii Min <innokentii_min@epam.com>
2021-02-08 14:40:13 +0300

    Fix mlflow_to_gppi_cli() function path for cli script

--- mlflow/setup.py
+++ mlflow/setup.py
@@ -61,7 +61,7 @@ setup(
             'odahu-flow-mlflow-runner=odahuflow.trainer.mlflow_full.runner:main',
             'odahu-flow-mlflow-project-runner=odahuflow.trainer.mlflow_projects.runner:main',
             'odahu-flow-mlflow-wrapper=odahuflow.trainer.helpers.wrapper.wrapper:main'
-            'odahu-flow-mlflow-gppi-converter=odahuflow.mlflowrunner.runner:mlflow_to_gppi_cli',
+            'odahu-flow-mlflow-gppi-converter=odahuflow.trainer.mlflow_helper:mlflow_to_gppi_cli',
         ],
     },
     install_requires=requirements,
