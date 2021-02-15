5742dd0accc453889ddb28d22ac7b60376b9562f
BPylypenko <49116723+BPylypenko@users.noreply.github.com>
2021-02-15 21:12:21 +0200

    fix. --tgz flag -> bool cannot be passed through cli

--- mlflow/odahuflow/trainer/helpers/mlflow_helper.py
+++ mlflow/odahuflow/trainer/helpers/mlflow_helper.py
@@ -173,7 +173,7 @@ def mlflow_to_gppi(model_meta: ModelIdentity, mlflow_model_path: str, gppi_model
     with open(project_file_path, 'w') as proj_stream:
         yaml.dump(manifest.dict(), proj_stream)
 
-    logging.info("GPPI stored. Start to GPPI validation")
+    logging.info("GPPI stored. Starting GPPI validation")
     mb = GPPITrainedModelBinary(gppi_model_path)
     mb.self_check()
     logging.info("GPPI is validated. OK")
@@ -273,10 +273,11 @@ def mlflow_to_gppi_cli():
                         help='Path to source MLFlow model directory')
     parser.add_argument('--gppi-model-path', '--gppi', required=True,
                         type=dir_type, help='Path to result GPPI model directory')
-    parser.add_argument('--tgz', required=False, default=True,
-                        type=bool, help='Make tar arhieve with gppi folder')
-
+    parser.add_argument('--tgz', dest='tgz', action='store_true', help='Make tar arhieve with gppi folder')
+    parser.add_argument('--no-tgz', dest='tgz', action='store_false')
+    parser.set_defaults(tgz=True)
     args = parser.parse_args()
+
     setup_logging(args)
     gppi_model_path: str = args.gppi_model_path
 
